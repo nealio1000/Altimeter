a117bff1da9b4e247c1d6d0168fdb122d953661f
Neal Friedman <nealiof1000@gmail.com>
2016-04-29 14:55:08 -0600

    Waiting on Samsung for SDK

--- app/src/main/java/ventureindustries/altimeter/HeartRateActivity.java
+++ app/src/main/java/ventureindustries/altimeter/HeartRateActivity.java
@@ -1,13 +1,62 @@
 package ventureindustries.altimeter;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 
-public class HeartRateActivity extends AppCompatActivity {
+import java.util.List;
+
+public class HeartRateActivity extends Activity {
+    private Sensor mHeartRateSensor;
+    private SensorManager mSensorManager;
+    private SensorEventListener mSensorEventListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_heart_rate);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        List<Sensor> deviceSensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+        mHeartRateSensor = mSensorManager.getDefaultSensor(65562);
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE) != null) {
+            mHeartRateSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_HEART_RATE);
+            Log.d("SENSORS", "HEART RATE SENSOR FOUND");
+            mSensorEventListener = new SensorEventListener() {
+                @Override
+                public void onSensorChanged(SensorEvent event) {
+
+                }
+
+                @Override
+                public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+                }
+            };
+        }
+        else{
+            Log.d("SENSORS", "NO HEART RATE SENSOR FOUND");
+        }
+
+        int breakhere = 1;
+    }
+
+    @Override
+    protected void onResume() {
+        // Register a listener for the sensor.
+        super.onResume();
+        mSensorManager.registerListener(mSensorEventListener, mHeartRateSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    protected void onPause() {
+        // Be sure to unregister the sensor when the activity pauses.
+        super.onPause();
+        mSensorManager.unregisterListener(mSensorEventListener);
     }
 }
