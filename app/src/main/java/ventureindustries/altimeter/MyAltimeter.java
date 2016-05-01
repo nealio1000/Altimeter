51226f854885164e043747550bd5571d93784fd1
Neal Friedman <nealiof1000@gmail.com>
2016-04-30 21:00:46 -0600

    Added in a class from NASA for calculating earth shape offsets for accurate GPS altitude

--- app/src/main/java/ventureindustries/altimeter/MyAltimeter.java
+++ app/src/main/java/ventureindustries/altimeter/MyAltimeter.java
@@ -1,16 +1,22 @@
 package ventureindustries.altimeter;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
@@ -18,11 +24,16 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+
 import java.io.IOException;
 import java.util.ArrayList;
+
+import gov.nasa.worldwind.geom.Angle;
+import gov.nasa.worldwind.geom.LatLon;
 
 
 public class MyAltimeter extends AppCompatActivity {
@@ -45,41 +56,44 @@ public class MyAltimeter extends AppCompatActivity {
     private Intent heartRateIntent;
     private Intent settingsIntent;
     private float unit = 3.28084f;
+    private EGM96 egm96;
+    private boolean isAltitudeModeGps = false;
+    private static final String dataFilePath = "WW15MGH.DAC";
+
+
+    private LocationManager locationManager;
+    private Location startLocation = null;
+    private Location lastKnown = null;
+    private LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_altimeter);
+        appInit();
+        startButtonListeners();
+        initializeBarometer();
+        initializeGps();
 
-        graphIntent = new Intent(this, GraphActivity.class);
-        heartRateIntent = new Intent(this, HeartRateActivity.class);
-        settingsIntent = new Intent(this, SettingsActivity.class);
+    }
 
-        startButton = (Button) findViewById(R.id.start_button);
-        stopButton = (Button) findViewById(R.id.stop_button);
-        getDataButton = (Button)findViewById(R.id.get_data_button);
-        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        mAltimeter = (TextView) findViewById(R.id.altitudeView);
-        heartRateButton = (Button) findViewById(R.id.heart_rate_button);
-        readyText = (TextView) findViewById(R.id.ready_text);
-        pressureText = (TextView) findViewById(R.id.pressure_text);
-        startButton.setEnabled(false);
-        stopButton.setEnabled(false);
-        elevations = new ArrayList<>();
-
-
-        if (mSensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE) != null){
+    private void initializeBarometer() {
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE) != null) {
             mBarometerSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
             Log.d(DEBUG_TAG, "Barometer found");
             mBarometerEventListener = new SensorEventListener() {
                 @Override
                 public void onSensorChanged(SensorEvent event) {
-                    if(pressureIsCurrent){
-                        float altitude = mSensorManager.getAltitude(currentSeaLevelPressure, event.values[0]) * unit;
-                        if(recordElevationFlag){
-                            elevations.add(altitude);
+
+                    if(!isAltitudeModeGps) {
+
+                        if (pressureIsCurrent) {
+                            float altitude = mSensorManager.getAltitude(currentSeaLevelPressure, event.values[0]) * unit;
+                            if (recordElevationFlag) {
+                                elevations.add(altitude);
+                            }
+                            mAltimeter.setText(String.valueOf(altitude));
                         }
-                        mAltimeter.setText(String.valueOf(altitude));
                     }
                 }
 
@@ -88,13 +102,56 @@ public class MyAltimeter extends AppCompatActivity {
 
                 }
             };
-        }
-        else {
+        } else {
             Log.d(DEBUG_TAG, "No barometer found");
         }
 
 
+    }
 
+    public void initializeGps() {
+
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+                if(isAltitudeModeGps) {
+                    if (isBetterLocation(location, lastKnown)) {
+                        LatLon latLon = LatLon.fromDegrees(location.getLatitude(), location.getLongitude());
+                        double offset = egm96.getOffset(latLon.getLatitude(), latLon.getLongitude());
+
+
+                        if (recordElevationFlag) {
+                            lastKnown = location;
+                        }
+                        mAltimeter.setText(String.valueOf(lastKnown.getAltitude()));
+                    }
+                }
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+            }
+        };
+
+        if (ActivityCompat.checkSelfPermission(
+                getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5, locationListener);
+    }
+
+
+    public void startButtonListeners() {
         getDataButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -102,9 +159,9 @@ public class MyAltimeter extends AppCompatActivity {
             }
         });
 
-        startButton.setOnClickListener(new View.OnClickListener(){
+        startButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view){
+            public void onClick(View view) {
                 elevations.clear();
                 startButton.setEnabled(false);
                 stopButton.setEnabled(true);
@@ -123,7 +180,7 @@ public class MyAltimeter extends AppCompatActivity {
 
                 float[] data = new float[elevations.size()];
                 int i = 0;
-                for(Float f : elevations){
+                for (Float f : elevations) {
                     data[i++] = (f != null ? f : Float.NaN);
                 }
 
@@ -138,7 +195,30 @@ public class MyAltimeter extends AppCompatActivity {
                 startActivity(heartRateIntent);
             }
         });
+    }
 
+    public void appInit() {
+        try {
+            egm96 = new EGM96(dataFilePath);
+        } catch (IOException e) {
+
+        }
+        graphIntent = new Intent(this, GraphActivity.class);
+        heartRateIntent = new Intent(this, HeartRateActivity.class);
+        settingsIntent = new Intent(this, SettingsActivity.class);
+
+        startButton = (Button) findViewById(R.id.start_button);
+        stopButton = (Button) findViewById(R.id.stop_button);
+        getDataButton = (Button) findViewById(R.id.get_data_button);
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAltimeter = (TextView) findViewById(R.id.altitudeView);
+        heartRateButton = (Button) findViewById(R.id.heart_rate_button);
+        readyText = (TextView) findViewById(R.id.ready_text);
+        pressureText = (TextView) findViewById(R.id.pressure_text);
+        startButton.setEnabled(false);
+        stopButton.setEnabled(false);
+        elevations = new ArrayList<>();
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
     }
 
     @Override
@@ -146,16 +226,30 @@ public class MyAltimeter extends AppCompatActivity {
 
         if (resultCode == RESULT_OK && requestCode == 0) {
             Bundle bundle = data.getExtras();
+
             boolean isBarometerSelected = bundle.getBooleanArray("settings")[0];
             boolean isFeetSelected = bundle.getBooleanArray("settings")[1];
 
-            if(isBarometerSelected){
+            if (isBarometerSelected) {
                 mSensorManager.registerListener(mBarometerEventListener, mBarometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
 
                 // Stop GPS
+                if (ActivityCompat.checkSelfPermission(
+                        getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                locationManager.removeUpdates(locationListener);
+
+                isAltitudeModeGps = false;
             }else{
                 mSensorManager.unregisterListener(mBarometerEventListener);
                 // Start GPS
+                if (ActivityCompat.checkSelfPermission(
+                        getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5, locationListener);
+                isAltitudeModeGps = true;
             }
 
             if(isFeetSelected)
@@ -219,21 +313,19 @@ public class MyAltimeter extends AppCompatActivity {
     protected void onResume() {
         // Register a listener for the sensor.
         super.onResume();
-        mSensorManager.registerListener(mBarometerEventListener, mBarometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
+//        mSensorManager.registerListener(mBarometerEventListener, mBarometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
     @Override
     protected void onPause() {
         // Be sure to unregister the sensor when the activity pauses.
         super.onPause();
-        mSensorManager.unregisterListener(mBarometerEventListener);
+//        mSensorManager.unregisterListener(mBarometerEventListener);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-
         getMenuInflater().inflate(R.menu.my_altimeter_actions, menu);
-
         return true;
     }
 
@@ -241,19 +333,56 @@ public class MyAltimeter extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.settings:
-
                 startActivityForResult(settingsIntent, 0);
-
-
                 return true;
-
             default:
                 // If we got here, the user's action was not recognized.
                 // Invoke the superclass to handle it.
                 return super.onOptionsItemSelected(item);
+        }
+    }
 
+    private static final int TWO_MINUTES = 1000 * 60 * 2;
+
+    /** Determines whether one Location reading is better than the current Location fix
+     * @param location  The new Location that you want to evaluate
+     * @param currentBestLocation  The current Location fix, to which you want to compare the new one
+     */
+    protected boolean isBetterLocation(Location location, Location currentBestLocation) {
+        if (currentBestLocation == null) {
+            // A new location is always better than no location
+            return true;
         }
 
+        // Check whether the new location fix is newer or older
+        long timeDelta = location.getTime() - currentBestLocation.getTime();
+        boolean isSignificantlyNewer = timeDelta > TWO_MINUTES;
+        boolean isSignificantlyOlder = timeDelta < -TWO_MINUTES;
+        boolean isNewer = timeDelta > 0;
 
+        // If it's been more than two minutes since the current location, use the new location
+        // because the user has likely moved
+        if (isSignificantlyNewer) {
+            return true;
+            // If the new location is more than two minutes older, it must be worse
+        } else if (isSignificantlyOlder) {
+            return false;
+        }
+
+        // Check whether the new location fix is more or less accurate
+        int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation.getAccuracy());
+        boolean isLessAccurate = accuracyDelta > 0;
+        boolean isMoreAccurate = accuracyDelta < 0;
+        boolean isSignificantlyLessAccurate = accuracyDelta > 200;
+
+        // Determine location quality using a combination of timeliness and accuracy
+        if (isMoreAccurate) {
+            return true;
+        } else if (isNewer && !isLessAccurate) {
+            return true;
+        } else if (isNewer && !isSignificantlyLessAccurate) {
+            return true;
+        }
+        return false;
     }
 }
